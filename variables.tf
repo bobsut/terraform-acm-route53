0e9f6a82e276e20c4007406f4807850f1bbc952f
Bob Sutterfield <bobsut@users.noreply.github.com>
2023-02-10 11:42:32 -0800

    compact region list

--- variables.tf
+++ variables.tf
@@ -34,35 +34,10 @@ variable "region" {
       # data.aws_regions.all.names
       #
       # instead we use cli output
-      # aws ec2 describe-regions --all-regions \
+      # aws ec2 describe-regions --all-regions --output json \
       # --filter Name="opt-in-status",Values="opted-in","opt-in-not-required" \
-      # --query "Regions[].RegionName|sort(@)"
-      [
-        "af-south-1",
-        "ap-east-1",
-        "ap-northeast-1",
-        "ap-northeast-2",
-        "ap-northeast-3",
-        "ap-south-1",
-        "ap-southeast-1",
-        "ap-southeast-2",
-        "ap-southeast-3",
-        "ca-central-1",
-        "eu-central-1",
-        "eu-central-2",
-        "eu-north-1",
-        "eu-south-1",
-        "eu-west-1",
-        "eu-west-2",
-        "eu-west-3",
-        "me-central-1",
-        "me-south-1",
-        "sa-east-1",
-        "us-east-1",
-        "us-east-2",
-        "us-west-1",
-        "us-west-2"
-      ]
+      # --query "Regions[].RegionName|sort(@)" | jq --compact-output
+      ["af-south-1", "ap-east-1", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", "ca-central-1", "eu-central-1", "eu-central-2", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3", "me-central-1", "me-south-1", "sa-east-1", "us-east-1", "us-east-2", "us-west-1", "us-west-2"]
     , var.region)
     error_message = "You can't deploy in region ${var.region}."
   }
